d12f2d7c82a8f6e2b7740ddf1f3367698219d50c
Andrea Parodi <andrea@parro.it>
2017-12-06 22:44:34 +0100

    first version

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-export default async function aiDecode(data) {
-  for (const item of data) {
-    await item;
-  }
+import AsyncIterable from "asynciterable";
+import { TextDecoder } from "util";
+
+export default function aiDecode(iterable, encoding = "utf8") {
+  const decoder = new TextDecoder(encoding);
+  const generator = iterable[Symbol.asyncIterator] || iterable[Symbol.iterator];
+  const iterator = generator.call(iterable);
+
+  return new AsyncIterable(async (write, end) => {
+    let item = {};
+    while (!item.done) {
+      item = await iterator.next();
+      if (!item.done) {
+        write(decoder.decode(item.value, { stream: true }));
+      }
+    }
+    const lastChunk = decoder.decode();
+    if (lastChunk) {
+      write(lastChunk);
+    }
+    end();
+  });
 }
--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 import test from "tape-async";
 import aiDecode from ".";
+import { fs } from "ai-node";
+import concat from "ai-concat";
+
+const fixture = `${__dirname}/fixtures/test`;
 
 test("exports a function", async t => {
-  t.is(typeof aiDecode, "function");
+  t.is(typeof fs.readFile, "function");
+});
+
+test("decode an iterable", async t => {
+  const string = await concat.obj(aiDecode(fs.readFile(fixture)));
+  t.deepEqual(string, ["test line 1\nline 2\n"]);
 });
