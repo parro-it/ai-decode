331c44cb0e9ca4dca166429ed587f7a3477c5bca
Andrea Parodi <andrea@parro.it>
2017-12-06 22:47:28 +0100

    docs

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import AsyncIterable from "asynciterable";
 import { TextDecoder } from "util";
 
+/**
+ * Decode a Buffer async iterable into a String async iterable.
+ * @param  {AsyncIterable} iterable The source iterable to decode. It must emit Buffer instances
+ * @param  {String} encoding The encoding to use for the conversion. Default to 'utf8'
+ * @return {AsyncIterable}          An async iterable that iterates over the decode String s
+ */
 export default function aiDecode(iterable, encoding = "utf8") {
   const decoder = new TextDecoder(encoding);
   const generator = iterable[Symbol.asyncIterator] || iterable[Symbol.iterator];
